0bfe5918086071daad8950134e15b6b0aee8206f
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-10-29 16:21:31 +0900

    05

--- main.c
+++ main.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 5
+#define ROWS 3
+#define COLS 3
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS]);
+void printMatrix(int A[][COLS]);
 
-int main(int argc, char *argv[]) {
+int main(void) {
 	
-	int i;
-	int grade[SIZE];
-	int score[SIZE];
+	int A[ROWS][COLS] = { 
+		{ 2, 3, 4 },
+		{ 8, 9, 1 },
+		{ 7, 0, 5 }
+	};
 	
-	for(i=0; i<SIZE; i++){
-		grade[i] = rand()%100;
-	}
+	int B[ROWS][COLS] = { 
+		{ 1, 0, 0 },
+		{ 0, 1, 0 },
+		{ 0, 0, 1 }
+	};
 	
-	for(i=0; i<SIZE; i++){
-		score[i]=grade[i];
-	}
-	for(i=0; i<SIZE; i++){
-		printf("score[%d] %i (%i)\n", i, score[i], grade[i]);
-	}
+	int C[ROWS][COLS];
+	
+	addMatrix(A, B, C);
+	printMatrix(C);
+	
 	return 0;
 	
 }
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS]) {
+	int i, j;
+	
+		for(i=0; i<ROWS; i++) {
+		for(j=0; j<COLS; j++) {
+			C[i][j] = A[i][j] + B[i][j];
+		}
+	}
+}
+
+void printMatrix(int A[][COLS]) {
+	int i, j;
+	for(i=0; i<ROWS; i++) {
+		for(j=0; j<COLS; j++) {
+			printf("%3d", A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
