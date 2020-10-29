bdaaff4d4bad69c8ca05b26755f32b0eb43d4d77
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-10-29 15:23:19 +0900

    01

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	int grade[5];
+	
+	grade[0] = 10;
+	grade[1] = 20;
+	grade[2] = 30;
+	grade[3] = 40;
+	grade[4] = 50;
+	
+	for(i=0; i<5; i++){
+		printf("student %i's grade :  %i\n", i, grade[i]);
+	}
+	
+	return 0;
+}
